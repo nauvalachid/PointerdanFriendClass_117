458accd6cb588d777bbf087fcaf78c07fb4bdecb
nauvalachid <nauvalachid@gmail.com>
2025-05-06 19:29:16 +0700

    Menambahkan class 'siswa' dan mengimplementasikan akses data private class 'orang' melalui friend class

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -12,3 +12,35 @@ class orang
         void setNama(string nama);
         friend class siswa;
 };
+
+class siswa
+{
+    private:
+    int id;
+
+    public:
+    void setId(int pId);
+    void displayall(orang &a);
+};
+
+void siswa::displayall(orang &a)
+{
+    cout << id << endl << a.nama ;
+}
+void orang::setNama(string pnama)
+{
+    nama = pnama;
+}
+void siswa::setId(int pid)
+{
+    id = pid;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Widodo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayall(joko);
+    return 0;
+}
