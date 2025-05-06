656df49c695590593d33eed4d635d2a8a9508924
nauvalachid <nauvalachid@gmail.com>
2025-05-06 18:49:25 +0700

    membuat pointer dereference pMhs

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -16,4 +16,9 @@ int main()
     mahasiswa &refMhs = mhs; //Pointer Reference refMhs
     refMhs.nim = 2; //Member Access Operator
     mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //Pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
 }
